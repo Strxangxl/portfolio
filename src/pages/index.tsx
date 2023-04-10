dbf8547bcc8c9bf55bb28b891cdb4aa289ac72c4
Ratandeep <chaddharatandeepsingh2345@gmail.com>
2023-04-11 00:59:48 +0530

    edits in meta head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
 import HeroSection from "@/components/HeroSection";
 import { Box } from "@chakra-ui/react";
+import Head from "next/head";
 
 const Home = () => {
   return (
-    <Box marginTop={-24}>
-      <HeroSection />
-    </Box>
+    <>
+      <Head>
+        <title>strxangxl</title>
+        <meta name="description" content="Portfolio site of strxangxl" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Box marginTop={-24}>
+        <HeroSection />
+      </Box>
+    </>
   );
 };
 
