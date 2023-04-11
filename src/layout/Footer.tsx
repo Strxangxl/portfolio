d8c73033ecf1f9c230272f8dcbdf1126ba8a0adb
Ratandeep <chaddharatandeepsingh2345@gmail.com>
2023-04-11 20:06:44 +0530

    sanity installation done

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <Divider />
 
       <Text fontSize={['xs', 'sm']}>
-        &copy; 2023 - {new Date().getFullYear()}{' '}
+        &copy; 2023 - {new Date().getFullYear() + 1}{' '}
         <Link href="https://github.com/Strxangxl" target="_blank">
           Ratandeep Singh
         </Link>
