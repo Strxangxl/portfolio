f5038bb3060a6a8ae30b5d1499c40fbf4dd991df
Ratandeep <chaddharatandeepsingh2345@gmail.com>
2023-04-16 11:47:45 +0530

    contentlayer folder remove from gitignore

---
+++ .contentlayer/.cache/v0.3.1/compiled-contentlayer-config-RAQTTBZI.mjs
@@ -0,0 +1,10 @@
+// contentlayer.config.ts
+import { makeSource } from "contentlayer/source-files";
+var contentlayer_config_default = makeSource({
+  contentDirPath: "content",
+  documentTypes: []
+});
+export {
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-RAQTTBZI.mjs.map
---
+++ .contentlayer/.cache/v0.3.1/compiled-contentlayer-config-V2SIO5TO.mjs
@@ -0,0 +1,31 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+var Project = defineDocumentType(() => ({
+  name: "Project",
+  filePathPattern: "projects/*.md",
+  fields: {
+    title: { type: "string", required: true },
+    description: { type: "string", required: true },
+    published: { type: "boolean" },
+    // will be shown in home
+    highlight: { type: "boolean" },
+    // shown at project list frontpage
+    featured: { type: "boolean" },
+    stacks: { type: "list", of: { type: "string" } },
+    projectLink: { type: "string" }
+  },
+  computedFields: {
+    id: {
+      type: "string",
+      resolve: (post) => post._raw.sourceFileName.replace(/\.md|\.mdx$/, "")
+    }
+  }
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "content",
+  documentTypes: []
+});
+export {
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-V2SIO5TO.mjs.map
