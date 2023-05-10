8023901dfb63709c3d9aefd6236cc3f224ecd2aa
Ratandeep <chaddharatandeepsingh2345@gmail.com>
2023-05-10 22:15:01 +0530

    framer-motion setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,20 @@ import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import theme from "../styles/theme";
 import Layout from "@/layout";
+import { motion } from "framer-motion";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1.0 }}
+    >
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </motion.div>
   );
 }
