b042cc63b3cc9c47efcf8288d004822ee72e923a
Ratandeep Singh <singhratandeep2345@gmail.com>
2026-07-02 15:17:38 +0530

    feat: fresh start with javascript boilerplate

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,6 @@
-import { withContentlayer } from "next-contentlayer"
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+};
 
-export default withContentlayer({})
+export default nextConfig;
--- src/types/types.ts
+++
@@ -1,9 +0,0 @@
-import type { IconType } from "react-icons";
-
-export type MotionLinkProps = {
-  name: string;
-  url: string;
-  icon: IconType;
-};
-
-export default MotionLinkProps;
